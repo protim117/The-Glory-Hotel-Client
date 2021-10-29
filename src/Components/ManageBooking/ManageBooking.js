2c69b0b4fef64e51f3940a5b627c1bf54a174ed5
Pro Tim <protimcbpiyas@gmail.com>
2021-10-30 01:22:47 +0600

    cleaning unnecessary code

--- src/Components/ManageBooking/ManageBooking.js
+++ src/Components/ManageBooking/ManageBooking.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState,useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import ManageBookingTable from './ManageBookingTable';
 import './ManageBooking.css'
 
